dcba5d4023b04072fd87a8c71a0b81816ab8a647
Thomas Bell <thomasjbell065@gmail.com>
2022-12-04 21:01:00 -0500

    styling / props
    added color and styles to sheet as well as moved components into their own files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-
 import "./App.css";
 import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-          monsters: [],
-          searchField: ''
+            monsters: [],
+            searchField: "",
         };
-        console.log("Life cycle method: 1-constructor");
+        //console.log("Life cycle method: 1-component");
     }
 
     // First time component is rendered
-  
+
     componentDidMount() {
-        console.log("Life cycle method: 3-componentDidMount");
+        //console.log("Life cycle method: 3-componentDidMount");
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
             .then((users) =>
@@ -30,33 +31,30 @@ class App extends Component {
             );
     }
 
-    render() {
-      console.log("Life cycle method: 2-render");
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLowerCase();
+        this.setState(() => {
+            return { searchField };
+        });
+    };
 
-      const filteredMonsters = this.state.monsters.filter((monster) => {
-          return monster.name.toLowerCase().includes(this.state.searchField);
-      });
+    render() {
+        // console.log("Life cycle method: 2-render");
+        const { monsters, searchField } = this.state;
+        const { onSearchChange } = this;
+
+        const filteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLowerCase().includes(searchField);
+        });
 
         return (
             <div className="App">
-                <input
-                    className="search-box"
-                    type="search"
-                    placeholder="Search Monsters"
-                    onChange={(event) => {
-                        const searchField = event.target.value.toLowerCase();
-                        this.setState(() => {
-                            return { searchField };
-                        });
-                    }}
+                <SearchBox
+                    className="monsters-search-box"
+                    onChangeHandler={onSearchChange}
+                    placeholder={"search monsters"}
                 />
-                {filteredMonsters.map((monster) => {
-                    return (
-                        <div key={monster.id}>
-                            <h1>{monster.name}</h1>
-                        </div>
-                    );
-                })}
+                <CardList monsters={filteredMonsters} />
             </div>
         );
     }
