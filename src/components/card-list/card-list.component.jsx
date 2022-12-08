7e5b327507f29c0b7c45cea2d69813ac6c231eea
Thomas Bell <thomasjbell065@gmail.com>
2022-12-07 21:04:19 -0500

    classes
    changing over class and destructuring

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 
+import Card from "../card/card.component";
 import "./card-list.styles.css";
 
 class CardList extends Component {
@@ -10,17 +11,7 @@ class CardList extends Component {
         return (
             <div className="card-list">
                 {monsters.map((monster) => {
-                    const { name, email, id } = monster;
-                    return (
-                        <div className="card-container" key={id}>
-                            <img
-                                alt={`monster ${name}`}
-                                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                            />
-                            <h2>{name}</h2>
-                            <p>{email}</p>
-                        </div>
-                    );
+                    return <Card monster={monster} />;
                 })}
             </div>
         );
