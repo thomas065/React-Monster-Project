fc1a955669cd0ef6479a653d1b0659d9549d34fd
Thomas Bell <thomasjbell065@gmail.com>
2022-12-08 19:59:17 -0500

    functional components
    updated my class components to functional components and destructured them accordingly.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,103 @@
 import "./App.css";
-import { Component } from "react";
+// import { Component } from "react";
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            monsters: [],
-            searchField: "",
-        };
-        //console.log("Life cycle method: 1-component");
-    }
+const App = () => {
+    const [searchField, setSearchField] = useState("");
+    const [monsters, setMonsters] = useState([]);
+    const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-    componentDidMount() {
-        //console.log("Life cycle method: 3-componentDidMount");
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
-            .then((users) =>
-                this.setState(
-                    () => {
-                        return { monsters: users };
-                    },
-                    () => {
-                        console.log(this.state);
-                    }
-                )
-            );
-    }
+            .then((users) => setMonsters(users));
+    }, []); // We only want this fetch to happen once unless the data changes
 
-    onSearchChange = (event) => {
-        const searchField = event.target.value.toLowerCase();
-        this.setState(() => {
-            return { searchField };
-        });
+    const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLowerCase();
+        setSearchField(searchFieldString);
     };
 
-    render() {
-        // console.log("Life cycle method: 2-render");
-        const { monsters, searchField } = this.state;
-        const { onSearchChange } = this;
-
-        const filteredMonsters = monsters.filter((monster) => {
+    useEffect(() => {
+        const newFilteredMonsters = monsters.filter((monster) => {
             return monster.name.toLowerCase().includes(searchField);
         });
+        setFilteredMonsters(newFilteredMonsters);
+    }, [monsters, searchField]);
 
-        return (
-            <div className="App">
-                <h1 className="app-title">React Monster Rolodex</h1>
+    return (
+        <div className="App">
+            <h1 className="app-title">React Monster Rolodex</h1>
 
-                <SearchBox
-                    className="monsters-search-box"
-                    onChangeHandler={onSearchChange}
-                    placeholder={"search monsters"}
-                />
-                <CardList monsters={filteredMonsters} />
-            </div>
-        );
-    }
-}
+            <SearchBox
+                className="monsters-search-box"
+                onChangeHandler={onSearchChange}
+                placeholder={"search monsters"}
+            />
+            <CardList monsters={filteredMonsters} />
+        </div>
+    );
+};
 
 export default App;
+
+// class App extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             monsters: [],
+//             searchField: "",
+//         };
+//         //console.log("Life cycle method: 1-component");
+//     }
+
+// componentDidMount() {
+//     //console.log("Life cycle method: 3-componentDidMount");
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//         .then((response) => response.json())
+//         .then((users) =>
+//             this.setState(
+//                 () => {
+//                     return { monsters: users };
+//                 },
+//                     () => {
+//                         console.log(this.state);
+//                     }
+//                 )
+//             );
+//     }
+
+// onSearchChange = (event) => {
+//     const searchField = event.target.value.toLowerCase();
+//     this.setState(() => {
+//         return { searchField };
+//     });
+// };
+
+//     render() {
+//         // console.log("Life cycle method: 2-render");
+//         const { monsters, searchField } = this.state;
+//         const { onSearchChange } = this;
+
+//         const filteredMonsters = monsters.filter((monster) => {
+//             return monster.name.toLowerCase().includes(searchField);
+//         });
+
+//         return (
+//             <div className="App">
+//                 <h1 className="app-title">React Monster Rolodex</h1>
+
+//                 <SearchBox
+//                     className="monsters-search-box"
+//                     onChangeHandler={onSearchChange}
+//                     placeholder={"search monsters"}
+//                 />
+//                 <CardList monsters={filteredMonsters} />
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,21 +1,33 @@
-import { Component } from "react";
+// import { Component } from "react";
 
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-    render() {
-        // console.log(this.props.monsters);
-        // console.log('render from CardList');
-        const { monsters } = this.props;
-        return (
-            <div className="card-list">
-                {monsters.map((monster) => {
-                    return <Card monster={monster} />;
-                })}
-            </div>
-        );
-    }
-}
+const CardList = ({ monsters }) => {
+    return (
+        <div className="card-list">
+            {monsters.map((monster) => {
+                return <Card monster={monster} />;
+            })}
+        </div>
+    );
+};
 
 export default CardList;
+
+// class CardList extends Component {
+//     render() {
+//         // console.log(this.props.monsters);
+//         // console.log('render from CardList');
+//         const { monsters } = this.props;
+//         return (
+//             <div className="card-list">
+//                 {monsters.map((monster) => {
+//                     return <Card monster={monster} />;
+//                 })}
+//             </div>
+//         );
+//     }
+// }
+
+// export default CardList;
